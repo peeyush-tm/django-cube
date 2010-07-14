b877818084f69682894c98cf91ceb6227498a870
spiq <spiq@localhost>
2010-07-14 11:08:15 +0300

    base rewritten + docs rewritten + tests run

--- cube/base.py
+++ cube/base.py
@@ -25,21 +25,44 @@ from .utils import odict
 
 
 class BaseDimension(object):
+    """
+    The base class for a dimension of a cube.
+    """
+
     def __init__(self, sample_space=[]):
+        """
+        :param sample_space: The sample space of the dimension to create.
+        """
         self._name = None
         self.sample_space = sample_space
         self._constraint = None
 
     @property
     def name(self):
+        """
+        str -- The name of the dimension.
+        """
         return self._name
 
     @property
     def constraint(self):
+        """
+        object -- The value to which the dimension is constrained
+        """
         return self._constraint
 
-    def constrain(self, value):
+    @constraint.setter
+    def constraint(self, value):
+        """
+        Setter for the property :meth:`constraint`.
+        """
         self._constraint = value
+
+    def get_sample_space(self):
+        """
+        :returns: list -- The sorted sample space for the calling dimension. 
+        """
+        return self._sort_sample_space(self.sample_space)
 
     def _sort_sample_space(self, sspace):
         """
@@ -48,31 +71,41 @@ class BaseDimension(object):
         """
         return sorted(list(sspace))
 
-    def get_sample_space(self):
-        """
-        :returns: list -- The sorted sample space of *dimension* for the calling cube. 
-        """
-        return self._sort_sample_space(self.sample_space)
-
     def __copy__(self):
+        """
+        Realizes a shallow copy of the dimension.
+        """
         sample_space = copy.copy(self.sample_space)
         dimension_copy = Dimension(sample_space=sample_space)
         dimension_copy._name = self._name
         return dimension_copy
 
+class BaseCubeOptions(object):
+    """
+    'Container' object for meta informations on a cube class. 
+    """
+    def __init__(self):
+        self.dimensions = None
+
 class BaseCubeMetaclass(type):
-    #Metaclass for BaseCube
+    """
+    Metaclass for :class:`BaseCube`.
+    """
     def __new__(cls, name, bases, attrs):
-        #dictionnary containing dimensions
+        #meta informations on a cube class
+        attrs['_meta'] = BaseCubeOptions()
+
+        #We gather in *dimensions* all the dimensions found in *attrs*
         dimensions = {}
         attrs_copy = copy.copy(attrs)
         for attr_name, attr_value in attrs_copy.iteritems():
             if isinstance(attr_value, BaseDimension):
                 dimensions[attr_name] = attr_value
                 attr_value._name = attr_name
+                #we don't want the dimension to be an attribute of *BaseCube*
                 del attrs[attr_name]
-        attrs['dimensions'] = dimensions
 
+        attrs['_meta'].dimensions = dimensions
         return super(BaseCubeMetaclass, cls).__new__(cls, name, bases, attrs)
         
 class BaseCube(object):
@@ -84,58 +117,40 @@ class BaseCube(object):
 
     def __new__(cls, *args, **kwargs):
         """
-        Used to give the instance local copies of the dimensions declared at the class level.
+        Provides the instance with local copies of the dimensions declared at the class level.
         """
         new_cube = super(BaseCube, cls).__new__(cls)
         
-        #overrides the dimensions from the class with local copies 
-        new_cube.dimensions = {}
-        for dim_name, dimension in cls.dimensions.iteritems():
-            dim_copy = copy.copy(dimension)
-            new_cube.dimensions[dim_name] = dim_copy
+        #overrides the dimensions from the class with local copies
+        new_cube.dimensions = copy.deepcopy(cls._meta.dimensions)
         
         return new_cube
-
-    def __init__(self):
-        """
-        """
-        self.dimensions = copy.copy(self.dimensions)
      
     def subcubes(self, *dim_names):
         """
-        Return an ordered list of all the sucubes of the calling cube, dimensions with names in *dim_names* constrained to all the possible values in their sample spaces. For example :
+        Return an ordered iterator, on all the sucubes with dimensions in *dim_names* constrained. For example :
 
-            >>> sample_space = {
-            ...     'name': ['John', 'Jack'],
-            ...     'instrument': ['Trumpet'],
-            ...     'age': [14, 89],
-            ... }
-            >>> Cube(['name', 'instrument', 'age'], len, sample_space=sample_space).subcubes('name', 'instrument')
-            [Cube(age, name='John', instrument='Trumpet'), Cube(age, name='Jack', instrument='Trumpet')]
+            >>> class MyCube(Cube):
+            ...     name = Dimension(sample_space=['John', 'Jack'])
+            ...     instrument = Dimension(sample_space=['Trumpet'])
+            ...     age = Dimension(sample_space=[14, 89])
 
-        If one of the dimensions passed as parameter is already constrained in the calling cube, it is not considered as an error. The sample space taken for this dimension will merely be a singleton : the constraint value. 
+            >>> list(MyCube().subcubes('name', 'instrument'))
+            [Cube(age, instrument='Trumpet', name='Jack'), Cube(age, instrument='Trumpet', name='John')]
+
+        .. note:: If one of the dimensions whose name passed as parameter is already constrained in the calling cube, it is not considered as an error.
         """
-        dim_names = list(copy.copy(dim_names))
+        dim_names = list(dim_names)
+        free_dim_name = self._pop_first_dim(dim_names, free_only=True)
 
-        for index, dim_name in enumerate(dim_names):
-            if dim_name not in self.dimensions:
-                raise ValueError("invalid dimension %s" % dim_name)
-            #if dimension is constrained we don't need to iterate for it.
-            if dim_name in self.constraint:
-                dim_names.pop(index)
-
-        if len(dim_names):
-            #we fix a dimension,
-            fixed_dim_name = dim_names.pop()
-            #and create a subcube with the remaining free dimensions,
-            #one for each value in the fixed dimension's sample space.
-            #Every one of these cubes is constrained *fixed_dim_name=value*
-            for value in self.get_sample_space(fixed_dim_name):
-                #subcube_constraint = cube_constraint + extra_constraint
-                extra_constraint = {fixed_dim_name: value}
-                #constrained subcube
+        if free_dim_name:
+            #We create one subcube for each value in the free dimension's sample space.
+            #Every one of these cubes is constrained *free_dim_name=value*
+            for value in self.get_sample_space(free_dim_name):
+                #subcube constraint = cube constraint + extra constraint
+                extra_constraint = {free_dim_name: value}
                 subcube = self.constrain(**extra_constraint)
-                #we yield all the measures for the constrained cube
+                #we yield all the subcubes of the constrained subcube with the remaining dimensions
                 for subsubcube in subcube.subcubes(*dim_names):
                     yield subsubcube
             raise StopIteration
@@ -147,21 +162,19 @@ class BaseCube(object):
 
     def constrain(self, **extra_constraint):
         """
-        Merges the calling cube's constraint with *extra_constraint*.
+        Merges (or overrides) the calling cube's constraint with *extra_constraint*.
 
-        :returns: Cube -- a subcube of the calling cube.
-
-        .. todo:: remove in the future ?
+        :returns: Cube -- a subcube of the calling cube, with the new constraint.
         """
-        dimensions = copy.deepcopy(self.dimensions)
+        cube_copy = copy.copy(self)
+        dimensions = cube_copy.dimensions
+
         for dim_name, value in extra_constraint.iteritems():
             try:
-                dimensions[dim_name].constrain(value)
+                dimensions[dim_name].constraint = value
             except KeyError:
                 raise ValueError("invalid dimension %s" % dim_name)
-                
-        cube_copy = copy.copy(self)
-        cube_copy.dimensions = dimensions
+        
         return cube_copy
 
     def measure(self, **coordinates):
@@ -169,14 +182,15 @@ class BaseCube(object):
         Calculates and returns the measure on the cube at *coordinates*. For example :
             
             >>> cube.measure(dim1=val1, dim2=val2, dimN=valN)
+            12.98
 
         If *coordinates* is empty, the measure returned is calculated on the whole cube. 
         """
         raise NotImplementedError
 
-    def measure_dict(self, *free_dim_names, **kwargs):
+    def measure_dict(self, *dim_names, **kwargs):
         """
-        Returns an ordered dictionnary of measures from the cube, structured following *free_dim_names*. For example :
+        Returns an ordered dictionnary of measures from the cube, structured following *dim_names*. For example :
 
             >>> cube(['dim1', 'dim2']).measures_dict('dim2', 'dim1') == {
             ...     'subcubes': {
@@ -196,7 +210,7 @@ class BaseCube(object):
             ...     'measure': measure
             ... }
 
-        If *full=False*, only the measures for which all dimensions in *free_dim_names* are fixed will be returned. For example : ::
+        If *full=False*, only the measures for which all dimensions in *dim_names* are fixed will be returned. For example : ::
 
             >>> cube(['dim1', 'dim2']).measures_dict('dim2', 'dim1', full=False) == {
             ...     dim2_val1: {
@@ -212,14 +226,16 @@ class BaseCube(object):
         """
         full = kwargs.setdefault('full', True)
         returned_dict = odict()
-        #if free dim_names, we have to fix one, and iterate over the subcubes. 
-        if free_dim_names:
-            free_dim_names = list(free_dim_names)
-            fixed_dimension = free_dim_names.pop(0)
+
+        dim_names = list(dim_names)
+        next_dim_name = self._pop_first_dim(dim_names)
+
+        if next_dim_name:
+            #dictionnary containing *measure_dict* of the subcubes
             subcubes_dict = odict()
-            for subcube in self.subcubes(fixed_dimension):
-                dim_value = subcube.constraint[fixed_dimension]
-                subcubes_dict[dim_value] = subcube.measure_dict(*free_dim_names, **kwargs)
+            for subcube in self.subcubes(next_dim_name):
+                dim_value = subcube.constraint[next_dim_name]
+                subcubes_dict[dim_value] = subcube.measure_dict(*dim_names, **kwargs)
             if full:
                 returned_dict['measure'] = self.measure()
                 returned_dict['subcubes'] = subcubes_dict
@@ -229,9 +245,9 @@ class BaseCube(object):
             returned_dict['measure'] = self.measure()
         return returned_dict
 
-    def measure_list(self, *free_dim_names):
+    def measure_list(self, *dim_names):
         """
-        Returns a multidimensionnal list of measures from the cube, structured following *free_dim_names*. For example : ::
+        Returns a multidimensionnal list of measures from the cube, structured following *dim_names*. For example : ::
 
             >>> cube(['dim1', 'dim2']).measures_list('dim2', 'dim1') == [
             ...     [measure_11_21, measure_11_22, , measure_11_2N],
@@ -241,34 +257,54 @@ class BaseCube(object):
             ... ] # Where <measure_AB_CD> means measure of cube with dimA=valB and dimC=valD
         """
         returned_list = []
-        if free_dim_names:
-            free_dim_names = list(free_dim_names)
-            fixed_dimension = free_dim_names.pop(0)
-        else:
-            return [self.measure()]
-         
-        if free_dim_names:
-            for subcube in self.subcubes(fixed_dimension):
-                returned_list.append(subcube.measure_list(*free_dim_names))
-        else:
-            for subcube in self.subcubes(fixed_dimension):
+
+        dim_names = list(dim_names)
+        next_dim_name = self._pop_first_dim(dim_names)
+        
+        #We check if there is still dimensions in *dim_names*,
+        #otherwise we return a list of measures. 
+        if dim_names:
+            for subcube in self.subcubes(next_dim_name):
+                returned_list.append(subcube.measure_list(*dim_names))
+        elif next_dim_name:
+            for subcube in self.subcubes(next_dim_name):
                 returned_list.append(subcube.measure())
         return returned_list
 
     def get_sample_space(self, dim_name):
         """
+        :returns: list -- the sample space for the cube for the dimension *dim_name*. 
         """
         return self.dimensions[dim_name].get_sample_space()
 
     @property
     def constraint(self):
         """
+        dict -- a dictionnary *dimension_name: constraint_value*. The dimensions that are not constrained are not in this dictionnary.
         """
         constraint_dict = {}
         for dimension in self.dimensions.values():
             if dimension.constraint:
                 constraint_dict[dimension.name] = dimension.constraint
         return constraint_dict
+
+    def _pop_first_dim(self, dim_names, free_only=False):
+        """
+        Pops the first dimension name from *dim_names*.
+
+        :param free_only: if True, only the dimensions that are not constrained will be poped.
+
+        :returns: The poped dimension name, or None if there is no dimension name to pop.
+        """
+        for index, dim_name in enumerate(dim_names):
+            if dim_name not in self.dimensions:
+                raise ValueError("invalid dimension %s" % dim_name)
+            #if dimension is constrained we don't need to iterate for it.
+            if free_only and dim_name in self.constraint:
+                continue
+            else:
+                return dim_names.pop(index)
+        return None
 
     def __copy__(self):
         """
@@ -282,5 +318,4 @@ class BaseCube(object):
     def __repr__(self):
         constr_dimensions = sorted(["%s=%s" % (dim, value) for dim, value in self.constraint.iteritems()])
         free_dimensions = sorted(list(set(self.dimensions) - set(self.constraint)))
-
         return 'Cube(%s)' % ", ".join(free_dimensions + constr_dimensions)
