9951430f107c0f5134c32f0d97ab4523cd5fd67f
sebpiq <sebpiq@localhost>
2010-11-04 16:03:05 +0200

    simplified the use of view 'table_from_cube', put the template as default, updated some doc

--- cube/views.py
+++ cube/views.py
@@ -1,10 +1,23 @@
-from django.views.generic.simple import direct_to_template
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
-def table_from_cube(request, cube, dimensions, **kwargs):
+def table_from_cube(request, cube=None, dimensions=None, extra_context={}, template_name='table_from_cube.html'):
     """
-    A view built on *direct_to_template*, that adds an extra_context built with :func:`table_from_cube_context`.
+    A view that renders *template_name* with a context built with :func:`cube.models.Cube.table_helper`.
+
+    Kwargs:
+    
+        cube(Cube). The cube to build the table from.
+        dimensions(list). A list ["dimension1", "dimension2"], where "dimension1" is the name of the dimension that will be used for columns, "dimension2" the name of the dimension for rows.
     """
-    extra_context = cube.table_helper(*dimensions)
-    extra_context["cube"] = cube
-    kwargs.setdefault('extra_context', {}).update(extra_context)
-    return direct_to_template(request, **kwargs)
+    if not cube:
+        raise TypeError('You must provide a cube.')
+
+    if not dimensions or None in dimensions:
+        raise TypeError('You must provide two dimensions, either by passing them as kwargs, or by sending them along with the request.')
+
+    context = cube.table_helper(*dimensions)
+    context["cube"] = cube
+    context.update(extra_context)
+
+    return render_to_response(template_name, context, context_instance=RequestContext(request))
